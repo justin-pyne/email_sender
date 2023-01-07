f1afb1c2b499ff0355de7600ee1f6c63d56124f1
justin-pyne <jpyne.justin@gmail.com>
2023-01-06 18:58:10 -0800

    finished email sender

---
+++ python_app.py
@@ -0,0 +1,36 @@
+from email.message import EmailMessage
+from app2 import password
+import ssl
+import smtplib
+
+
+#variables
+email_sender = "18pynejustin@gmail.com"
+email_password = password
+
+email_receiver = "mixet97888@letpays.com"
+
+subject = "Test subject"
+body = """
+Testing the body paragraph of an email.
+"""
+
+
+
+#creating EmailMessage instance
+em = EmailMessage()
+
+em['From'] = email_sender
+em['To'] = email_receiver
+em['subject'] = subject
+em.set_content(body)
+
+
+#sending the email
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
+
+
